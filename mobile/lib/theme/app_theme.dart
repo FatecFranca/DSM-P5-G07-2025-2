94a1cdcee2914884b14a32db9edcba3720aec3da
luizfelipesoarees <luizfelipesoarees@gmail.com>
2025-10-03 20:18:48 -0300

    correção do componente button

--- mobile/lib/theme/app_theme.dart
+++ mobile/lib/theme/app_theme.dart
@@ -4,14 +4,15 @@ import 'package:google_fonts/google_fonts.dart';
 class AppTheme {
   static ThemeData lightTheme = ThemeData(
     textTheme: GoogleFonts.poppinsTextTheme(Typography.blackCupertino),
-    scaffoldBackgroundColor: const Color(0xFFFAEDCB),
+    scaffoldBackgroundColor: const Color(0xFFF0D37B),
     colorScheme: ColorScheme.fromSeed(
-      seedColor: const Color(0xFFFAEDCB),
+      seedColor: const Color(0xFFF0D37B),
     ).copyWith(
-      primary: const Color(0xFFF39200),
+      primary: const Color(0xFFBF4904),
+      secondary: const Color(0xFFF39200),
     ),
     appBarTheme: const AppBarTheme(
-      backgroundColor: Color(0xFFF39200),
+      backgroundColor: Color(0xFFBF4904),
       titleTextStyle: TextStyle(
         color: Colors.white,
         fontSize: 20,
