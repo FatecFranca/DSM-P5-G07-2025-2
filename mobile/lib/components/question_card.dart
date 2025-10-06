a555c4c226bfbca95f2cc90a91a5d3f289a7b8ef
luizfelipesoarees <luizfelipesoarees@gmail.com>
2025-10-06 19:21:40 -0300

    correção componente QuestionCard

--- mobile/lib/components/question_card.dart
+++ mobile/lib/components/question_card.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:PetDex/theme/app_theme.dart';
 
 class QuestionCard extends StatelessWidget {
   final int questionNumber;
@@ -14,35 +16,39 @@ class QuestionCard extends StatelessWidget {
   Widget build(BuildContext context) {
     final colorScheme = Theme.of(context).colorScheme;
 
-    return Container(
-      width: double.infinity,
-      padding: const EdgeInsets.all(16),
-      margin: const EdgeInsets.symmetric(vertical: 8),
-      decoration: BoxDecoration(
-        color: colorScheme.surface,
-        borderRadius: BorderRadius.circular(16),
-      ),
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          Text(
-            "Pergunta $questionNumber",
-            style: TextStyle(
-              fontSize: 18,
-              fontWeight: FontWeight.bold, 
-              color: colorScheme.secondary,
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 32, vertical: 8), 
+      child: Container(
+        width: double.infinity,
+        padding: const EdgeInsets.symmetric(vertical: 20, horizontal: 16),
+        decoration: BoxDecoration(
+          color: AppColors.sand300,
+          borderRadius: BorderRadius.circular(16),
+        ),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            Text(
+              "Pergunta $questionNumber",
+              textAlign: TextAlign.center,
+              style: GoogleFonts.poppins(
+                fontSize: 18,
+                fontWeight: FontWeight.bold,
+                color: colorScheme.secondary,
+              ),
             ),
-          ),
-          const SizedBox(height: 8),
-          Text(
-            questionText,
-            style: const TextStyle(
-              fontSize: 16,
-              color: Colors.black87,
-              height: 1.4,
+            const SizedBox(height: 10),
+            Text(
+              questionText,
+              textAlign: TextAlign.center,
+              style: GoogleFonts.poppins(
+                fontSize: 16,
+                color: AppColors.black400,
+                height: 1.4,
+              ),
             ),
-          ),
-        ],
+          ],
+        ),
       ),
     );
   }
