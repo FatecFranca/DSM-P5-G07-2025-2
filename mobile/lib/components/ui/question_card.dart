b838db06e8177e9376f58f78ad23d3d5320a56ed
Gabriel Resende Spirlandelli <grspirlandelli@gmail.com>
2025-10-07 07:50:13 -0300

    Fix

--- mobile/lib/components/ui/question_card.dart
+++ mobile/lib/components/ui/question_card.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
-import 'package:PetDex/theme/app_theme.dart';
+import '../../theme/app_theme.dart';
 
 class QuestionCard extends StatelessWidget {
   final int questionNumber;
