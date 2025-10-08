f00b33d8d502a0a5e862e23116a94df6d0d71a1a
luizfelipesoarees <luizfelipesoarees@gmail.com>
2025-10-08 20:34:25 -0300

    adicionar componente NavBar

--- mobile/lib/main.dart
+++ mobile/lib/main.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:PetDex/theme/app_theme.dart';
-import 'package:PetDex/components/ui/answer_card.dart';
+import 'package:PetDex/components/ui/nav_bar.dart';
 
 void main() {
   runApp(const MyApp());
@@ -13,27 +13,9 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'PetDex',
+      debugShowCheckedModeBanner: false,
       theme: AppTheme.lightTheme,
-      home: const AnswerCardDemoPage(),
-    );
-  }
-}
-
-class AnswerCardDemoPage extends StatelessWidget {
-  const AnswerCardDemoPage({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(title: const Text("Testando o componente AnswerCard")),
-      body: Center(
-        child: AnswerCard(
-          initialValue: "",
-          onAnswerChanged: (resposta) {
-            debugPrint("Usuário digitou: $resposta");
-          },
-        ),
-      ),
+      home: const NavBar(), // ✅ Apenas isso!
     );
   }
 }
