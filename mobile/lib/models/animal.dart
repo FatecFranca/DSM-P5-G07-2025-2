fc2fb11420c0f7dbc60759ba615f15221765f845
Gabriel Resende Spirlandelli <grspirlandelli@gmail.com>
2025-10-13 20:41:29 -0300

    Correção da classe Models/Animal

--- mobile/lib/models/animal.dart
+++ mobile/lib/models/animal.dart
@@ -1,19 +1,44 @@
-class Animal {
-  final String name;
-  final String sex;
-  final String imageUrl;
+import 'dart:ffi';
 
-  Animal({required this.name, required this.sex, required this.imageUrl});
+import 'package:PetDex/data/enums/species.dart';
+import 'package:intl/intl.dart';
+
+class Animal {
+  final String id;
+  final String nome;
+  final DateFormat dataNascimento;
+  final String sexo;
+  final Float peso;
+  final Bool castrado;
+  final String usuario;
+  final String raca;
+  final SpeciesEnum especie;
+
+  Animal({
+    required this.id,
+    required this.nome,
+    required this.dataNascimento,
+    required this.sexo,
+    required this.peso,
+    required this.castrado,
+    required this.usuario,
+    required this.raca,
+    required this.especie,
+  });
 
   factory Animal.fromJson(Map<String, dynamic> json) {
-    // A API retorna uma lista de fotos, vamos pegar a primeira
-    final List<dynamic> images = json['urlImage'] ?? [];
-    final String imageUrl = images.isNotEmpty ? images.first['url'] : 'https://i.imgur.com/sopaE59.png'; 
-
     return Animal(
-      name: json['name'] ?? 'Pet',
-      sex: json['sex'] ?? 'NA',
-      imageUrl: imageUrl,
+      id: json['id'],
+      nome: json['nome'],
+      dataNascimento: json['dataNascimento'],
+      sexo: json['sexo'],
+      peso: json['peso'],
+      castrado: json['castrado'],
+      usuario: json['usuario'],
+      raca: json['racaNome'],
+      especie: json['especieNome'] == 'gato'
+          ? SpeciesEnum.cat
+          : SpeciesEnum.dog,
     );
   }
 }
