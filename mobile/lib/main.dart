694be1f7848f4d51dfd8be1e52697ebc61aa4f03
Gabriel Resende Spirlandelli <grspirlandelli@gmail.com>
2025-10-18 19:51:38 -0300

    atualização main

--- mobile/lib/main.dart
+++ mobile/lib/main.dart
@@ -7,7 +7,7 @@ import 'package:flutter_dotenv/flutter_dotenv.dart';
 // Instância global do serviço de autenticação
 final authService = AuthService();
 
-void main() async {
+Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
 
   // Carrega variáveis de ambiente
@@ -26,52 +26,9 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'PetDex Component Test',
       debugShowCheckedModeBanner: false,
       theme: AppTheme.lightTheme,
-      home: const ComponentTestScreen(),
-    );
-  }
-}
-
-class ComponentTestScreen extends StatefulWidget {
-  const ComponentTestScreen({super.key});
-
-  @override
-  State<ComponentTestScreen> createState() => _ComponentTestScreenState();
-}
-
-class _ComponentTestScreenState extends State<ComponentTestScreen> {
-  DateTime _activeDate = DateTime.now();
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(title: const Text('Teste do SelectDate')),
-      body: Center(
-        child: Padding(
-          padding: const EdgeInsets.all(16.0),
-          child: Column(
-            mainAxisSize: MainAxisSize.min,
-            children: [
-              SelectDate(
-                initialDate: _activeDate,
-                onDateSelected: (newDate) {
-                  setState(() {
-                    _activeDate = newDate;
-                  });
-                  print('Nova data selecionada: $newDate');
-                },
-              ),
-              const SizedBox(height: 30),
-              Text(
-                'Data ativa na tela: ${_activeDate.toLocal().toString().split(' ')[0]}',
-                style: const TextStyle(fontSize: 16),
-              ),
-            ],
-          ),
-        ),
-      ),
+      home: const AppShell(),
     );
   }
 }
